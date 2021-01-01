5e40ec94288481f1288c3f1676cb52917b7517e6
Monil Joshi <monil.joshi@qualitia.local>
2021-01-01 23:54:21 +0530

    Fundamentals of JavaScript Part 1 | Practice code for Type Conversion and Coercion

---
+++ Practice/TypeConversion_and_coercion.js
@@ -0,0 +1,10 @@
+
+
+const birthYear = '1991';
+
+// Type conversion
+console.log(birthYear + 18);
+console.log(Number(birthYear) + 18);
+
+// Type coercion
+console.log('I am ' + 23 + ' years old')
