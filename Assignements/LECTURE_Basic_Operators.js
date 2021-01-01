48cba565b6afc34c54f5ca581c3f8032009a7d82
Monil Joshi <monil.joshi@qualitia.local>
2021-01-01 20:11:19 +0530

    Fundamentals of JavaScript Part 1 | Assignment Basic operators

---
+++ Assignements/LECTURE_Basic_Operators.js
@@ -0,0 +1,12 @@
+const population_ = 13700000;
+splitted_population = population_ / 2;
+console.log(splitted_population);
+
+splitted_population++;
+console.log(splitted_population);
+
+const population_finland = 60000;
+console.log(population_finland > splitted_population)
+
+let avg_population = 3000000
+console.log(avg_population < splitted_population)
--- Assignements/LECTURE_Values_and_Variables.js
+++ Assignements/LECTURE_Values_and_Variables.js
@@ -1,6 +1,6 @@
 let country = 'India';
 let continent = 'Asia';
-let population = 1.37
+let population = 13700000
 
 console.log(country)
 console.log(continent)
