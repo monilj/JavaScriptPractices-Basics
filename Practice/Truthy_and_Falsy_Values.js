541b7a33d91a1dca9743080e2e73847921a9ae80
Monil Joshi <monil.joshi@qualitia.local>
2021-01-02 00:30:53 +0530

    Fundamentals of JavaScript Part 1 | Practice code for Truthy and Falsy values

---
+++ Practice/Truthy_and_Falsy_Values.js
@@ -0,0 +1,15 @@
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("NewVlue"));
+console.log(Boolean(null));
+console.log(Boolean({}));
+console.log(Boolean(''));
+
+
+const money = 0;
+if (money) {
+    console.log("Do not spend all ");
+}
+else {
+    console.log("You should earn money")
+}
