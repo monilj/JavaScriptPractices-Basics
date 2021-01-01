e863a53bee10a655fc749ea483696fd96b6c363b
Monil Joshi <monil.joshi@qualitia.local>
2021-01-01 19:49:39 +0530

    Fundamentals of JavaScript Part 1 | Assignment let, const, var

---
+++ Assignements/LECTURE_let_const_and_var.js
@@ -0,0 +1,11 @@
+const language_in_Maharashtra = 'Marathi';
+console.log(language_in_Maharashtra);
+
+let city = 'Bangalore';
+city = 'Pune';
+console.log(city);
+
+
+var laptop = 'Mac';
+laptop = 'windows';
+console.log(laptop);
