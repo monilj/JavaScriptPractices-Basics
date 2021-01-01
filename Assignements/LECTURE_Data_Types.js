845c6725150813cfb1a538deae29dde96e96b5e0
Monil Joshi <monil.joshi@qualitia.local>
2021-01-01 19:31:00 +0530

    Fundamentals of JavaScript Part 1 | Assignment Data Types

---
+++ Assignements/LECTURE_Data_Types.js
@@ -0,0 +1,12 @@
+let isIsland = false;
+let language;
+country = 'India';
+continent = 'Asia';
+population = 1.37
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+
