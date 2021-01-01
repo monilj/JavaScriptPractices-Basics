c85a22213a982aa3d41a45eef681195cf2df51d2
Monil Joshi <monil.joshi@qualitia.local>
2021-01-01 21:48:20 +0530

    Fundamentals of JavaScript Part 1 | Assignment Taking Decision if / else statement

---
+++ Assignements/LECTURE_Taking_Decisions_if_else_Statements.js
@@ -0,0 +1,11 @@
+const country = 'Portugal';
+const continent = 'Europe';
+const population = 1100000;
+const avg_population = 3300000;
+
+if (population > avg_population) {
+    console.log(`${country}'s population is above average`);
+}
+else {
+    console.log(`${country}'s population is below average`);
+}
