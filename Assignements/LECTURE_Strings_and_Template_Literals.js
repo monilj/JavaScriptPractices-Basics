d5138c11f77e3420201ab44b63a2f6dcce45c447
Monil Joshi <monil.joshi@qualitia.local>
2021-01-01 21:35:46 +0530

    Fundamentals of JavaScript Part 1 | Assignment for Strings and Template Literals

---
+++ Assignements/LECTURE_Strings_and_Template_Literals.js
@@ -0,0 +1,6 @@
+const country = 'Portugal';
+const continent = 'Europe';
+const population = '11 million';
+const language = 'portuguese';
+const descritption = `${country} is in ${continent} and its ${population} people speaks ${language}`;
+console.log(descritption)
