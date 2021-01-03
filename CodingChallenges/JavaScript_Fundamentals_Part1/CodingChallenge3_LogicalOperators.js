d930ddc71d74af7819cb57f660ecc63973b4d4ff
Monil Joshi <monil.joshi@qualitia.local>
2021-01-03 16:32:12 +0530

    Fundamentals of JavaScript Part 1 | Coding Challenge Logical Operator

---
+++ CodingChallenges/JavaScript_Fundamentals_Part1/CodingChallenge3_LogicalOperators.js
@@ -0,0 +1,51 @@
+
+//Case 1 
+// const doplphin = (96 + 108 + 89) / 3;
+// const Koalas = (101 + 111 + 110) / 3;
+
+// if (doplphin > Koalas) {
+//     console.log('doplphin is winner');
+// }
+// else if (doplphin < Koalas) {
+//     console.log('Koalas is winner');
+// }
+// else if (doplphin === Koalas) {
+//     console.log('Match is draw');
+// }
+
+
+//Case 2 
+
+// const doplphin = (97 + 112 + 101) / 3;
+// const Koalas = (109 + 95 + 123) / 3;
+
+// if (doplphin > Koalas && doplphin >= 100) {
+//     console.log('doplphin is winner');
+// }
+// else if (doplphin < Koalas && Koalas >= 100) {
+//     console.log('Koalas is winner');
+// }
+// else if (doplphin === Koalas && Koalas >= 100 && doplphin >= 100) {
+//     console.log('Match is draw');
+// }
+// else {
+//     console.log('No one is eligible to reach min score')
+// }
+
+// Case 3
+
+const doplphin = (97 + 112 + 86) / 3;
+const Koalas = (109 + 95 + 86) / 3;
+
+if (doplphin > Koalas && doplphin >= 100) {
+    console.log('doplphin is winner');
+}
+else if (doplphin < Koalas && Koalas >= 100) {
+    console.log('Koalas is winner');
+}
+else if (doplphin === Koalas && Koalas >= 100 && doplphin >= 100) {
+    console.log('Match is draw');
+}
+else {
+    console.log('No one is eligible to reach min score')
+}
