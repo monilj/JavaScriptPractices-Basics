269cf33eb3de0a1b80e2f8966b069f3623a0b319
Monil Joshi <monil.joshi@qualitia.local>
2021-01-03 15:17:07 +0530

    Fundamentals of JavaScript  Part 1 | Practice code for Logical operator

---
+++ Practice/Logical_Operator.js
@@ -0,0 +1,9 @@
+const hasDriverLicense = true;
+const hadVision = true;
+console.log(hasDriverLicense && hadVision);
+console.log(hasDriverLicense || hadVision);
+
+const hasDriverLicenses = false;
+const hadVisions = false;
+console.log(hasDriverLicense && hadVision);
+console.log(hasDriverLicense || hadVision);
