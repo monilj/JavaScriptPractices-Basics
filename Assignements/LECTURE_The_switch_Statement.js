158223512f66f0091328ef9269a1c08efdadc30a
Monil Joshi <monil.joshi@qualitia.local>
2021-01-03 17:12:35 +0530

    Fundamentals of JavaScript Part 1 | Assignment The Switch Statement

---
+++ Assignements/LECTURE_The_switch_Statement.js
@@ -0,0 +1,18 @@
+const language = 'Japanese';
+switch (language) {
+    case 'mandarin':
+    case 'chinese':
+        console.log('MOST number of native speakers');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'english':
+        console.log('3rd place in number of native speakers');
+        break;
+    case 'hindi':
+        console.log('4th place in number of native speakers');
+        break;
+    default:
+        console.log('good to know');
+}
