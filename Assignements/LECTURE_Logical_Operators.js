356e79ec154509d4a02bf8c5bb2b9af524c85b8b
Monil Joshi <monil.joshi@qualitia.local>
2021-01-03 16:11:59 +0530

    Fundamentals of JavaScript Part 1 | Assignment Logical operator

---
+++ Assignements/LECTURE_Logical_Operators.js
@@ -0,0 +1,10 @@
+language = "English";
+population = 3700000;
+isIsLand = false;
+
+if (language === 'English' && population < 5000000 && isIsLand === false) {
+    console.log('You should live in Portugal');
+}
+else {
+    console.log('Portugal does not meet your criteria')
+}
