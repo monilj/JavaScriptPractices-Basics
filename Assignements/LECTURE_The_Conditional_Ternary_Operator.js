e481e710e8505ef5550c9351180810305eebdaf5
Monil Joshi <monil.joshi@qualitia.local>
2021-01-04 08:19:10 +0530

    Fundamentals of JavaScript Part 1 | The Conditional Ternary Operator

---
+++ Assignements/LECTURE_The_Conditional_Ternary_Operator.js
@@ -0,0 +1,10 @@
+const population = 31000000;
+const country = 'Portugal'
+population > 3300000 ? console.log("Portugal's population is above average") :
+    console.log("Portugal's population is below average")
+
+console.log(`${country}'s population is ${population > 3300000 ? 'above' : 'below'} average`);
+
+
+const age = 12;
+console.log(`you are ${age >= 18 ? 'eligible' : 'not eligible'} to drive`);
